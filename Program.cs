700bc7731e7a173b269212f1541165a00dddf497
Onome-O <Owah-O@ulster.ac.uk>
2025-03-24 16:45:59 +0000

    Saving my local changes before pulling

--- Program.cs
+++ Program.cs
@@ -11,16 +11,17 @@ internal class Program
 
         // Configure Authentication / Authorisation via extension methods 
         builder.Services.AddCookieAuthentication();
-        //builder.Services.AddPolicyAuthorisation();
 
         builder.Services.AddDbContext<DatabaseContext>(options =>
         {
             // Configure connection string for selected database in appsettings.json
-
-            options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
-            //options.UseMySql(builder.Configuration.GetConnectionString("MySql"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySql")));
-            //options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"));
-            //options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+            var connectionString = builder.Configuration.GetConnectionString("Sqlite");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                // Fallback to environment variable in Render
+                connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+            }
+            options.UseSqlite(connectionString);
         });
 
         // Add UserService to DI   
@@ -36,40 +37,34 @@ internal class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
         else
         {
-       using (var scope = app.Services.CreateScope())
-    {
-        Seeder.Seed(
-            scope.ServiceProvider.GetService<IUserService>(), 
-            scope.ServiceProvider.GetService<IPatientService>()
-        );
-    }
-
+            using (var scope = app.Services.CreateScope())
+            {
+                Seeder.Seed(
+                    scope.ServiceProvider.GetService<IUserService>(), 
+                    scope.ServiceProvider.GetService<IPatientService>()
+                );
+            }
         }
 
-        //app.UseHttpsRedirection();
         app.UseStaticFiles();
-
         app.UseRouting();
-
-        // ** configure cors to allow full cross origin access to any webapi end points **
-        //app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-        // ** turn on authentication/authorisation **
         app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        // ** Render requires dynamic port handling **
+        var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+        app.Urls.Add($"http://0.0.0.0:{port}");
 
         app.Run();
     }
